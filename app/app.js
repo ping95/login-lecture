f2eeaf8125ba74a749bd7a0a12f190fbe5f4cc00
hyjung <ping95a@gmail.com>
2021-08-28 22:09:59 +0900

    public폴더 연결

--- app/app.js
+++ app/app.js
@@ -6,10 +6,11 @@ const app = express();
 
 // 라우팅
 const home = require("./src/routes/home")
-//  앱 셋팅
-app.set("views", "./views");
-app.set("view engine", "ejs");
 
+//  앱 셋팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`))
 
 app.use("/", home)   // use -> middleware를 등록해주는 메소드
 
