884c0acf99ba62eab8021db2930ffbbef9164861
hyjung <ping95a@gmail.com>
2021-08-31 22:43:42 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,41 +1,51 @@
 "use strict"
 
+const e = require("express");
+const fs = require("fs").promises;
+
 class UserStorage {
-    static #users = {
-        id: ["woorimIT", "나개발", "김팀장"],
-        psword: ["1234", "1234", "123456"],
-        name: ["우리밋", "나개발", "김팀장"]
-    };
-
-    static getUsers(...fields) {
-        const users = this.#users;
-        const newUsers = fields.reduce((newUsers, field) => {
-            if (users.hasOwnProperty(field)) {
-                newUsers[field] = users[field];
-            }
-            return newUsers;
-        }, {});
-        return newUsers;
-    };
-
-    static getUserInfo(id) {
-        const users = this.#users;
+    static #getUserInfo() {
+        const users = JSON.parse(data);
         const idx = users.id.indexOf(id);
         const usersKeys = Object.keys(users);
         const userInfo = usersKeys.reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
         }, {});
-
         return userInfo;
     }
 
+    static getUsers(...fields) {
+        fs.readFile("./src/databases/users.json", (err, data) => {
+            if (err) throw err;
+            const users = JSON.parse(data);
+            const newUsers = fields.reduce((newUsers, field) => {
+                if (users.hasOwnProperty(field)) {
+                    newUsers[field] = users[field];
+                }
+                return newUsers;
+            }, {});
+            return newUsers;
+        });
+    };
+
+    static getUserInfo(id) {
+        fs.readFile("./src/databases/users.json")
+            .then((data) => {
+                return this.#getUserInfo(data, id);
+            })
+            .catch(console.error);
+    }
+
     static save(userinfo) {
-        const users = this.#users;
-        users.id.push(userinfo.id);
-        users.name.push(userinfo.name);
-        users.psword.push(userinfo.psword);
-        return { success: true };
+        fs.readFile("./src/databases/users.json", (err, data) => {
+            if (err) throw err;
+            const users = JSON.parse(data);
+            users.id.push(userinfo.id);
+            users.name.push(userinfo.name);
+            users.psword.push(userinfo.psword);
+            return { success: true };
+        });
     }
 }
 
